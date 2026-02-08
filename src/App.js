024273d3e7325c746e2adf1f1e8f043fec833be6
laurapihamaa <laura.pihamaa@outlook.com>
2026-02-08 13:00:36 +0200

    test initializing the db in the pipeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { initAndCacheData } from './db/dataCache';
 
 function App() {
+
+  useEffect(() => {
+    initAndCacheData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
