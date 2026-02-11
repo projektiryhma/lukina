dcb000c37997964a5bf06e5345f69ac5cfae68ac
Juuso Suhonen <juuso.suhonen@tuni.fi>
2026-02-11 17:05:13 +0200

    style InfoPage difficulty buttons

--- src/pages/InfoPageGameOne.js
+++ src/pages/InfoPageGameOne.js
@@ -1,6 +1,13 @@
 import "./InfoPageGameOne.css"
+import { useNavigate } from 'react-router-dom';
 
 export function InfoPageGameOne(){
+
+    const navigate = useNavigate();
+
+    const handleButtonClick = (difficulty) => {
+        navigate("/GamePageGameOne", { state: { state: difficulty } });
+    };
 
     return(
 
@@ -9,13 +16,9 @@ export function InfoPageGameOne(){
 
             <p>Liirun laarum lopun</p>
 
-
-
-            <button className="DifButton">Helppo</button>
-            <button className="DifButton">Keskivaikea</button>
-            <button className="DifButton">Vaikea</button>
-
-
+            <button onClick={() => handleButtonClick('0') } className="DifButton">Helppo</button>
+            <button onClick={() => handleButtonClick('1') } className="DifButton">Keskivaikea</button>
+            <button onClick={() => handleButtonClick('2') } className="DifButton">Vaikea</button>
 
         </>
 
