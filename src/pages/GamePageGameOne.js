2f42dbd1f0f655a4b911a29d2ea37f5d1aa1c9b5
Juuso Suhonen <juuso.suhonen@tuni.fi>
2026-02-22 17:58:54 +0200

    setup gamepage

---
+++ src/pages/GamePageGameOne.js
@@ -0,0 +1,18 @@
+export function Game(){
+
+    const status = 0;
+
+    if (status = 0){
+
+        return(
+            <p>peli1</p>
+        )
+
+    }
+
+    
+    return(
+        <p>Peli2</p>
+    )
+
+}
