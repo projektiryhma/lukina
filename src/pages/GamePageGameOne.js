acc1a59484597ffd5cceae4c5bafce1b4065cb9a
Juuso Suhonen <juuso.suhonen@tuni.fi>
2026-02-25 12:47:58 +0200

    Make return system for different game phases

--- src/pages/GamePageGameOne.js
+++ src/pages/GamePageGameOne.js
@@ -1,9 +1,46 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export function GamePageGameOne() {
   const location = useLocation();
-
   const difficulty = location.state.state || "0";
 
-  return <p>peli {difficulty}</p>;
+  const [game, setGame] = useState(null);
+  const [isPhaseOne, setIsPhaseOne] = useState(true);
+
+  const fetchNewTask = async () => {
+    const data = "data";
+    setGame(data);
+    {
+      game;
+    }
+  };
+
+  useEffect(() => {
+    fetchNewTask();
+  }, [difficulty]);
+
+  const handleRestart = () => {
+    setIsPhaseOne(true);
+    fetchNewTask();
+  };
+
+  return (
+    <div className="game-page">
+      {isPhaseOne ? (
+        <button onClick={() => setIsPhaseOne(false)}>seuraava</button>
+      ) : (
+        <div className="phase-two">
+          <h2>Vaihe 2 </h2>
+          <button
+            onClick={() => {
+              handleRestart();
+            }}
+          >
+            Seuraava tehtävä
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
