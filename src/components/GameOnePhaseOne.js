f102eb9963ffd9d2f18389362a6f343953aef686
Juuso Suhonen <juuso.suhonen@tuni.fi>
2026-03-06 21:21:17 +0200

    make code simpler

--- src/components/GameOnePhaseOne.js
+++ src/components/GameOnePhaseOne.js
@@ -18,15 +18,7 @@ export function GameOnePhaseOne({ data }) {
         &lt; Edellinen
       </button>
       <h2 className="PhaseOneHeader">Etsi ja korjaa</h2>
-      <p
-        style={{
-          padding: "20px",
-          border: "1px solid #ccc",
-          background: "#f9f9f9",
-        }}
-      >
-        {text}
-      </p>
+      <p className="GameData">{text}</p>
       <p>
         Virheitä: <strong>{amountoferror}</strong>
       </p>
