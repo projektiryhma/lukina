6dba77d4e704903331965e359d9356dfa8e9034f
laurapihamaa <laura.pihamaa@outlook.com>
2026-03-26 18:57:21 +0200

    add tests for other lengths

--- cypress/e2e/InfoPage.cy.js
+++ cypress/e2e/InfoPage.cy.js
@@ -1,32 +1,60 @@
 /* eslint-disable no-undef */
-
-beforeEach(() => {});
-
-describe("Press button to move to game page with short sentences", () => {
-  it("renders", () => {
+describe("Press button to move to game page", () => {
+  it("short sentences", () => {
     cy.intercept("GET", "**/data/data.json", { fixture: "testdata.json" }).as(
       "fetchData",
     );
 
-    // Visit the page that triggers the fetch
     cy.visit("/#/InfoPageGameOne");
 
-    // Wait for the network request to happen
     cy.wait("@fetchData").wait(1000);
 
     cy.get(".DifButton").should("contain", "Lyhyt");
-    cy.get(".DifButton").should("contain", "Keskipitkä");
-    cy.get(".DifButton").should("contain", "Pitkä");
 
     cy.get(".DifButton").first().click();
 
-    cy.get(".GameData").should("contain", "Easy test item with error");
+    cy.get(".word-container").should("contain", "Easy");
+    cy.get(".word-container").should("contain", "error");
 
-    cy.get(".ErrorAmount")
-      .invoke("text")
-      .then((text) => {
-        const value = Number(text);
-        expect(value).to.be.within(1, 3);
-      });
+    cy.contains("Easy").click().should("have.class", "is-selected", "true");
+    cy.contains("error").click().should("have.class", "is-selected", "true");
+  });
+  it("medium sentences", () => {
+    cy.intercept("GET", "**/data/data.json", { fixture: "testdata.json" }).as(
+      "fetchData",
+    );
+
+    cy.visit("/#/InfoPageGameOne");
+
+    cy.wait("@fetchData").wait(1000);
+
+    cy.get(".DifButton").should("contain", "Keskipitkä");
+
+    cy.get(".DifButton").eq(1).click();
+
+    cy.get(".word-container").should("contain", "Medium");
+    cy.get(".word-container").should("contain", "error");
+
+    cy.contains("Medium").click().should("have.class", "is-selected", "true");
+    cy.contains("error").click().should("have.class", "is-selected", "true");
+  });
+  it("long sentences", () => {
+    cy.intercept("GET", "**/data/data.json", { fixture: "testdata.json" }).as(
+      "fetchData",
+    );
+
+    cy.visit("/#/InfoPageGameOne");
+
+    cy.wait("@fetchData").wait(1000);
+
+    cy.get(".DifButton").should("contain", "Pitkä");
+
+    cy.get(".DifButton").eq(2).click();
+
+    cy.get(".word-container").should("contain", "Hard");
+    cy.get(".word-container").should("contain", "error");
+
+    cy.contains("Hard").click().should("have.class", "is-selected", "true");
+    cy.contains("error").click().should("have.class", "is-selected", "true");
   });
 });
