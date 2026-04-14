07a36db947d1f85380c2cb91f4ace2aa3b699846
laurapihamaa <laura.pihamaa@outlook.com>
2026-04-14 18:34:09 +0300

    bring failing test back after fix

--- cypress/e2e/GamePageGameOne.cy.js
+++ cypress/e2e/GamePageGameOne.cy.js
@@ -74,10 +74,10 @@ describe("Press button check correct answers", () => {
       .contains("test")
       .click()
       .should("have.class", "is-selected", "true");
-    /*cy.get(".interactive-word")
+    cy.get(".interactive-word")
       .contains("error")
       .click()
-      .should("have.class", "is-selected", "true");*/
+      .should("have.class", "is-selected", "true");
 
     cy.contains("Näytä vihje").click();
 
@@ -95,9 +95,9 @@ describe("Press button check correct answers", () => {
     cy.get(".interactive-word")
       .contains("test")
       .should("have.class", "is-selected", "true");
-    /*cy.get(".interactive-word")
+    cy.get(".interactive-word")
       .contains("error")
-      .should("have.class", "is-selected", "true");*/
+      .should("have.class", "is-selected", "true");
   });
 
   it("zero selection", () => {
