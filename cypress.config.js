30c9428dfd9ba6c2076132e4d2b57bbd191b639a
laurapihamaa <laura.pihamaa@outlook.com>
2026-04-15 13:35:05 +0300

    clean up configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,51 +1,17 @@
 const { defineConfig } = require("cypress");
-const { GenerateCtrfReport } = require('cypress-ctrf-json-reporter')
-
-process.env.NODE_ENV = "development";
-const webpackConfig = require("react-scripts/config/webpack.config")(
-  "development"
-);
-
-const babelLoaderRule = webpackConfig.module.rules
-  .find((rule) => rule.oneOf)
-  .oneOf.find(
-    (rule) =>
-      rule.loader && rule.loader.includes("babel-loader") && rule.include
-  );
-
-if (babelLoaderRule) {
-  // Change include from just src/ to also include cypress/
-  const path = require("path");
-  babelLoaderRule.include = [
-    babelLoaderRule.include,
-    path.resolve(__dirname, "cypress"),
-  ];
-}
+const { GenerateCtrfReport } = require("cypress-ctrf-json-reporter");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
 
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "webpack",
-      webpackConfig,
-    },
-    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
-  },
-
   e2e: {
     baseUrl: "http://localhost:3000",
-    testIsolation: true,
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       new GenerateCtrfReport({
         on,
         outputFile: "ctrf-report-cypress.json",
       });
-      if (process.env.CYPRESS_COVERAGE === "true") {
-        require("@cypress/code-coverage/task")(on, config);
-      }
       return config;
     },
   },
